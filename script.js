7ccfd2985a1bbca5f350e4295c79cbe6ed01fdfd
jopeng5 <jopeng5@users.noreply.github.com>
2023-11-06 13:44:47 -0800

    Create operate function

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ let add = (num1, num2) => num1 + num2;
 let subtract = (num1, num2) => num1 - num2;
 let multiply = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
+
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function operate(num1, num2, operator) {
+  switch(operator) {
+    case 'add': 
+      add(num1, num2);
+      break;
+    case 'subtract': 
+      subtract(num1, num2);
+      break;
+    case 'multiply': 
+      multiply(num1, num2);
+      break;
+    case 'divide':
+      divide(num1, num2);
+      break;
+    default:
+      alert('error');
+  }
+}
